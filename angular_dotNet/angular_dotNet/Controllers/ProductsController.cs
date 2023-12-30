8db2cc20d303fb117aaf96ef60c349a6bd5ecd11
Ivan <ivan.stojanovic@ggstud.io>
2023-12-30 18:49:01 +0100

    setup

---
+++ angular_dotNet/angular_dotNet/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace angular_dotNet.Controllers
+{
+    [Route("[Controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetProducts()
+        {
+            return "this will be a list of products";
+        }
+
+        [HttpGet("{id}")]
+        public string GetProduct(byte id)
+        {
+            return $"this will be some product {id}";
+        }
+
+    }
+}
